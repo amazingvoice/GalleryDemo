7427f714e607c098e046500313dddbbbaebe688b
Qing Zhang <qz761@outlook.com>
2019-11-09 23:51:21 +0800

    add permission request

--- app/src/main/java/com/example/gallerydemo/MainActivity.java
+++ app/src/main/java/com/example/gallerydemo/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.gallerydemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, // permission group
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE); // request code
+
+            Log.d(TAG, "onCreate: FINISHED PERMISSION REQUEST AND GOT BACK HERE");
+        } else {
+            // Already got the permission
+            Log.d(TAG, "onCreate: ALREADY GOT THE PERMISSION");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted.
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
